672b9dcd6acff4cbadcb9af3111f0c834f105b16
SafCoder <76853684+SafCoder@users.noreply.github.com>
2023-01-29 09:51:11 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el");
+
+
+let count = 0;
+
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  let previousEntriesNums = count + ' - ' 
+  saveEl.textContent +=  previousEntriesNums
+  countEl.textContent = 0
+  count = 0
+
+
+  
+}
+ 
